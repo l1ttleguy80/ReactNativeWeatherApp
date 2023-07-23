6a4d9d99d3fb76d3475287df7df231817f95ec91
Neil <jbwalton@gmail.com>
2023-07-24 00:03:04 +0100

    Readme

---
+++ components/README.md
@@ -0,0 +1,19 @@
+# React Native Weather App
+
+First attempt at a React Native app. No previous experience with React or mobile development.
+
+This code is a bit more of a sandbox application pulled together from numerous experiments whilst watching and following lots of tutorials. It is not complete as I simply ran out of time. 
+
+Code written on my MacBook using the iPhone simulator for some testing. 
+
+There are a lot of areas I researched but there was an element of not trying to run before I could walk. However, these are some of things I was thinking about:
+
+SOLID Principles
+With C# backend development as my background, my first though on coding standards was the SOLID principles. This article in particular was useful: https://medium.com/docler-engineering/applying-solid-to-react-ca6d1ff926a4
+
+Unit Testing
+Unit tests for the React components is something I would have liked to have included based on this article: https://blog.logrocket.com/unit-testing-react-native/
+
+Further thoughts:
+Automated tests - Use Wiremock to mock API response
+UI - Not relevant to this app but can ION be used as a framework for the new app? 
--- components/WeatherForecast.js
+++ components/WeatherForecast.js
@@ -1,9 +1,33 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const WeatherForecast = () => {
   return (
-    <View style={{backgroundColor: '##323ea8', height: '100%'}}><Text>WeatherForecast</Text></View>
+    <View style={styles.container}>
+      <ForecastItem />
+      <ForecastItem />
+      <ForecastItem />
+      <ForecastItem />
+      <ForecastItem />
+    </View>
+  )
+}
+
+const ForecastItem = () => {
+  const img = {uri: 'https://openweathermap.org/img/wn/10d@2x.png'}
+  return (
+      <View style={styles.futureForecastItemContainer} >
+        <View style={styles.headerContainer}>
+          <Text style={styles.day}>Mon</Text>
+        </View>
+        <View style={styles.bodyContainer}>
+          <Image source={img} style={styles.image}/>
+          <View style={styles.forecastDetails}>
+            <Text style={styles.temp}>32&#176;C</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View> 
+      </View>
   )
 }
 
@@ -11,7 +35,45 @@ export default WeatherForecast
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '##323ea8',
-    height: '100%',
-  }
+    flex: 1,
+    flexDirection: 'column'
+  },
+image: {
+    width: 75,
+    height: 75
+},
+futureForecastItemContainer: {  
+  borderRadius: 10,
+  borderColor: '#eee',
+  borderWidth: 1,
+  marginTop: 10,
+  marginHorizontal: 15
+},
+headerContainer: {
+  alignItems: 'center',
+},
+bodyContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+},
+forecastDetails: {
+  textAlign: 'left'
+},
+day: {
+  fontSize: 30,
+  color: 'white',
+  textAlign: 'center',
+  fontWeight: '500',
+  marginBottom: 10
+},
+temp: {
+  fontSize: 20,
+  color: 'white',
+  fontWeight: '300',
+},
+description: {
+  fontSize: 16,
+  color: 'white',
+  fontWeight: '100',
+}
 })
